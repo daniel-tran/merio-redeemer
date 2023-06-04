90aab3cc811a057d21b624b6edec9dc4de0a4916
Daniel Tran <daniel.tran11@gmail.com>
2023-06-04 19:30:22 +1000

    Remove commented-out HUD components
    These were initially included as part of the initial boilerplate

--- src/js/stage/HUD.js
+++ src/js/stage/HUD.js
@@ -2,94 +2,8 @@ import { Container, game, event, Renderable, Vector2d, BitmapText, loader, level
 import { getDefaultFontSettings, isGameLevel, playBGM, resetFlashBlockData, resetScoreAndLives, resetAltModeSettings, hardResetScore } from './../renderables/entity-data.js';
 
 /**
- * a basic control to toggle fullscreen on/off
+ * a basic HUD item for mobile devices to simulate arrow keys
  */
-//class FSControl extends GUI_Object {
-//    /**
-//     * constructor
-//     */
-//    constructor(x, y) {
-//        super(x, y, {
-//            image: game.texture,
-//            region : "shadedDark30.png"
-//        });
-//        this.setOpacity(0.5);
-//    }
-//
-//    /**
-//     * function called when the pointer is over the object
-//     */
-//    onOver(/* event */) {
-//        this.setOpacity(1.0);
-//    }
-//
-//    /**
-//     * function called when the pointer is leaving the object area
-//     */
-//    onOut(/* event */) {
-//        this.setOpacity(0.5);
-//    }
-//
-//    /**
-//     * function called when the object is clicked on
-//     */
-//    onClick(/* event */) {
-//        if (!device.isFullscreen()) {
-//            device.requestFullscreen();
-//        } else {
-//            device.exitFullscreen();
-//        }
-//        return false;
-//    }
-//};
-
-/**
- * a basic control to toggle fullscreen on/off
- */
-//class AudioControl extends me.GUI_Object {
-//    /**
-//     * constructor
-//     */
-//    constructor(x, y) {
-//        super(x, y, {
-//            image: game.texture,
-//            region : "shadedDark13.png" // ON by default
-//        });
-//        this.setOpacity(0.5);
-//        this.isMute = false;
-//    }
-//
-//    /**
-//     * function called when the pointer is over the object
-//     */
-//    onOver(/* event */) {
-//        this.setOpacity(1.0);
-//    }
-//
-//    /**
-//     * function called when the pointer is leaving the object area
-//     */
-//    onOut(/* event */) {
-//        this.setOpacity(0.5);
-//    }
-//
-//    /**
-//     * function called when the object is clicked on
-//     */
-//    onClick(/* event */) {
-//        if (this.isMute) {
-//            me.audio.unmuteAll();
-//            this.setRegion(game.texture.getRegion("shadedDark13.png"));
-//            this.isMute = false;
-//        } else {
-//            me.audio.muteAll();
-//            this.setRegion(game.texture.getRegion("shadedDark15.png"));
-//            this.isMute = true;
-//        }
-//        return false;
-//    }
-//};
-
 class OnScreenControlButton extends GUI_Object {
     /**
      * constructor
@@ -284,14 +198,6 @@ class UIContainer extends Container {
                 input.KEY.RIGHT
             ));
         }
-
-        // add our audio control object
-        //this.addChild(new AudioControl(36, 56));
-
-        //if (!device.isMobile) {
-        //    // add our fullscreen control object
-        //    this.addChild(new FSControl(36 + 10 + 48, 56));
-        //}
     }
 };
 
